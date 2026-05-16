43c0623772d43149573a917554f7eb898a70cdbf
Daria Orasanu <dariaorasanu3@gmail.com>
2026-05-16 20:25:58 +0300

    Add more products to database

--- database/02_insert_data.sql
+++ database/02_insert_data.sql
@@ -111,22 +111,132 @@ INSERT INTO recomandari (id_chestionar, id_sport, scor_compatibilitate, nivel_re
 
 -- 6. PRODUSE
 
+-- 6. PRODUSE
+
+-- 6. PRODUSE
+-- Pentru fiecare sport avem produse pe nivel:
+-- 2 produse INCEPATOR, 2 produse MEDIU, 2 produse AVANSAT
+
 INSERT INTO produse (id_categorie, id_sport, denumire, pret, stoc, nivel_recomandat) VALUES
-                                                                                         (1, 1, 'Pantofi alergare basic', 199.99, 25, 'INCEPATOR'),
-                                                                                         (1, 1, 'Pantofi alergare performance', 399.99, 12, 'AVANSAT'),
-                                                                                         (9, 2, 'Ochelari inot anti-aburire', 89.99, 30, 'INCEPATOR'),
-                                                                                         (8, 3, 'Set gantere 2x5 kg', 149.99, 18, 'INCEPATOR'),
-                                                                                         (5, 4, 'Minge baschet indoor', 119.99, 20, 'INCEPATOR'),
-                                                                                         (5, 5, 'Minge volei recreativ', 99.99, 22, 'INCEPATOR'),
-                                                                                         (6, 6, 'Racheta tenis aluminiu', 249.99, 15, 'INCEPATOR'),
-                                                                                         (10, 7, 'Casca ciclism reglabila', 179.99, 14, 'MEDIU'),
-                                                                                         (11, 8, 'Saltea yoga antiderapanta', 129.99, 35, 'INCEPATOR'),
-                                                                                         (6, 9, 'Racheta badminton usoara', 89.99, 28, 'INCEPATOR'),
-                                                                                         (7, 10, 'Ham escalada indoor', 349.99, 8, 'AVANSAT'),
-                                                                                         (5, 11, 'Minge fotbal teren exterior', 139.99, 26, 'INCEPATOR'),
-                                                                                         (11, 12, 'Inel pilates flexibil', 59.99, 40, 'INCEPATOR'),
-                                                                                         (12, 13, 'Rucsac drumetie 20L', 229.99, 16, 'MEDIU'),
-                                                                                         (13, 14, 'Manusi box antrenament', 189.99, 19, 'MEDIU');
+-- 1. Alergare
+(1, 1, 'Pantofi alergare basic', 199.99, 25, 'INCEPATOR'),
+(4, 1, 'Bidon hidratare alergare', 39.99, 50, 'INCEPATOR'),
+(1, 1, 'Pantofi alergare confort', 299.99, 18, 'MEDIU'),
+(3, 1, 'Tricou alergare respirabil', 79.99, 30, 'MEDIU'),
+(1, 1, 'Pantofi alergare performance', 449.99, 10, 'AVANSAT'),
+(14, 1, 'Centura alergare cu buzunar', 89.99, 20, 'AVANSAT'),
+
+-- 2. Inot
+(9, 2, 'Casca inot silicon', 49.99, 40, 'INCEPATOR'),
+(9, 2, 'Ochelari inot basic', 69.99, 35, 'INCEPATOR'),
+(9, 2, 'Ochelari inot anti-aburire', 89.99, 25, 'MEDIU'),
+(14, 2, 'Geanta impermeabila inot', 119.99, 20, 'MEDIU'),
+(9, 2, 'Set inot avansat', 179.99, 12, 'AVANSAT'),
+(3, 2, 'Prosop sport microfibra', 99.99, 18, 'AVANSAT'),
+
+-- 3. Fitness acasa
+(8, 3, 'Banda elastica fitness', 39.99, 40, 'INCEPATOR'),
+(8, 3, 'Covoras fitness pliabil', 89.99, 25, 'INCEPATOR'),
+(8, 3, 'Set gantere 2x5 kg', 149.99, 20, 'MEDIU'),
+(8, 3, 'Roata abdomene', 69.99, 22, 'MEDIU'),
+(8, 3, 'Set fitness multifunctional', 349.99, 8, 'AVANSAT'),
+(8, 3, 'Banca fitness reglabila', 499.99, 6, 'AVANSAT'),
+
+-- 4. Baschet
+(5, 4, 'Minge baschet recreativ', 89.99, 30, 'INCEPATOR'),
+(3, 4, 'Tricou antrenament baschet', 79.99, 35, 'INCEPATOR'),
+(5, 4, 'Minge baschet indoor', 129.99, 20, 'MEDIU'),
+(7, 4, 'Manseta sport baschet', 39.99, 25, 'MEDIU'),
+(2, 4, 'Pantofi baschet performance', 399.99, 10, 'AVANSAT'),
+(5, 4, 'Minge baschet oficiala', 219.99, 12, 'AVANSAT'),
+
+-- 5. Volei
+(5, 5, 'Minge volei recreativ', 79.99, 25, 'INCEPATOR'),
+(3, 5, 'Tricou volei antrenament', 69.99, 30, 'INCEPATOR'),
+(7, 5, 'Genunchiere volei', 69.99, 30, 'MEDIU'),
+(5, 5, 'Minge volei sala', 129.99, 20, 'MEDIU'),
+(2, 5, 'Pantofi volei sala', 289.99, 12, 'AVANSAT'),
+(7, 5, 'Mansete volei', 89.99, 15, 'AVANSAT'),
+
+-- 6. Tenis
+(6, 6, 'Set mingi tenis', 49.99, 40, 'INCEPATOR'),
+(6, 6, 'Husa racheta tenis', 79.99, 22, 'INCEPATOR'),
+(6, 6, 'Racheta tenis aluminiu', 249.99, 18, 'MEDIU'),
+(3, 6, 'Tricou tenis respirabil', 89.99, 25, 'MEDIU'),
+(6, 6, 'Racheta tenis carbon', 599.99, 7, 'AVANSAT'),
+(2, 6, 'Pantofi tenis teren dur', 349.99, 10, 'AVANSAT'),
+
+-- 7. Cycling
+(4, 7, 'Bidon ciclism 750ml', 34.99, 45, 'INCEPATOR'),
+(10, 7, 'Manusi ciclism basic', 59.99, 30, 'INCEPATOR'),
+(10, 7, 'Manusi ciclism gel', 89.99, 25, 'MEDIU'),
+(10, 7, 'Casca ciclism reglabila', 179.99, 14, 'MEDIU'),
+(10, 7, 'Casca ciclism performanta', 249.99, 10, 'AVANSAT'),
+(3, 7, 'Tricou ciclism tehnic', 159.99, 12, 'AVANSAT'),
+
+-- 8. Yoga
+(11, 8, 'Bloc yoga din spuma', 39.99, 35, 'INCEPATOR'),
+(11, 8, 'Curea yoga pentru stretching', 29.99, 32, 'INCEPATOR'),
+(11, 8, 'Saltea yoga antiderapanta', 129.99, 28, 'MEDIU'),
+(11, 8, 'Perna yoga meditatie', 99.99, 20, 'MEDIU'),
+(11, 8, 'Set yoga premium', 249.99, 12, 'AVANSAT'),
+(11, 8, 'Saltea yoga profesionala', 299.99, 10, 'AVANSAT'),
+
+-- 9. Badminton
+(6, 9, 'Set fluturasi badminton', 34.99, 50, 'INCEPATOR'),
+(6, 9, 'Husa racheta badminton', 49.99, 20, 'INCEPATOR'),
+(6, 9, 'Racheta badminton usoara', 89.99, 28, 'MEDIU'),
+(2, 9, 'Pantofi badminton sala', 199.99, 14, 'MEDIU'),
+(6, 9, 'Racheta badminton carbon', 199.99, 14, 'AVANSAT'),
+(6, 9, 'Set badminton competitie', 299.99, 8, 'AVANSAT'),
+
+-- 10. Escalada
+(7, 10, 'Magneziu lichid escalada', 44.99, 25, 'INCEPATOR'),
+(7, 10, 'Saculet magneziu escalada', 59.99, 18, 'INCEPATOR'),
+(7, 10, 'Manusi escalada', 99.99, 16, 'MEDIU'),
+(2, 10, 'Incaltaminte escalada indoor', 299.99, 10, 'MEDIU'),
+(7, 10, 'Ham escalada indoor', 349.99, 8, 'AVANSAT'),
+(7, 10, 'Set carabiniere escalada', 189.99, 12, 'AVANSAT'),
+
+-- 11. Fotbal
+(5, 11, 'Minge fotbal recreativ', 79.99, 35, 'INCEPATOR'),
+(3, 11, 'Tricou antrenament fotbal', 69.99, 30, 'INCEPATOR'),
+(5, 11, 'Minge fotbal teren exterior', 139.99, 26, 'MEDIU'),
+(7, 11, 'Aparatori fotbal', 49.99, 28, 'MEDIU'),
+(2, 11, 'Ghete fotbal sintetic', 249.99, 12, 'AVANSAT'),
+(5, 11, 'Minge fotbal oficiala', 229.99, 15, 'AVANSAT'),
+
+-- 12. Pilates
+(11, 12, 'Inel pilates flexibil', 59.99, 40, 'INCEPATOR'),
+(11, 12, 'Banda elastica pilates', 39.99, 35, 'INCEPATOR'),
+(11, 12, 'Minge pilates', 49.99, 35, 'MEDIU'),
+(11, 12, 'Saltea pilates groasa', 149.99, 18, 'MEDIU'),
+(11, 12, 'Set pilates premium', 249.99, 12, 'AVANSAT'),
+(8, 12, 'Reformer pilates compact', 899.99, 4, 'AVANSAT'),
+
+-- 13. Drumetie
+(12, 13, 'Pelerina ploaie drumetie', 89.99, 30, 'INCEPATOR'),
+(4, 13, 'Sticla apa drumetie', 49.99, 35, 'INCEPATOR'),
+(12, 13, 'Bete trekking ajustabile', 159.99, 20, 'MEDIU'),
+(12, 13, 'Rucsac drumetie 20L', 229.99, 16, 'MEDIU'),
+(12, 13, 'Rucsac drumetie 30L', 299.99, 12, 'AVANSAT'),
+(2, 13, 'Bocanci drumetie', 399.99, 9, 'AVANSAT'),
+
+-- 14. Box
+(13, 14, 'Bandaje box elastice', 39.99, 40, 'INCEPATOR'),
+(13, 14, 'Coarda sarit box', 49.99, 30, 'INCEPATOR'),
+(13, 14, 'Manusi box antrenament', 189.99, 20, 'MEDIU'),
+(13, 14, 'Protectie dentara box', 34.99, 25, 'MEDIU'),
+(13, 14, 'Set box avansat', 399.99, 8, 'AVANSAT'),
+(13, 14, 'Casca protectie box', 229.99, 10, 'AVANSAT'),
+
+-- 15. Dans sportiv
+(3, 15, 'Tricou dans respirabil', 69.99, 25, 'INCEPATOR'),
+(14, 15, 'Geanta echipament dans', 99.99, 18, 'INCEPATOR'),
+(2, 15, 'Pantofi dans sportiv', 229.99, 12, 'MEDIU'),
+(3, 15, 'Colanti dans sportiv', 119.99, 20, 'MEDIU'),
+(2, 15, 'Pantofi dans competitie', 399.99, 7, 'AVANSAT'),
+(3, 15, 'Costum antrenament dans', 249.99, 10, 'AVANSAT');
 
 -- 7. COMENZI
 

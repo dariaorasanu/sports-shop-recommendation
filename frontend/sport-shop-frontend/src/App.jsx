b81a6a77bd5c180c170f7093fd39bded6c2aec30
Daria Orasanu <dariaorasanu3@gmail.com>
2026-05-16 12:44:55 +0300

    Add simple navigation between pages

--- frontend/sport-shop-frontend/src/App.jsx
+++ frontend/sport-shop-frontend/src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+import SportsPage from "./pages/SportsPage";
 import QuestionnairePage from "./pages/QuestionnairePage";
+import "./App.css";
 
 function App() {
-  return (
-      <div>
-        <QuestionnairePage />
-      </div>
-  );
+    const [currentPage, setCurrentPage] = useState("questionnaire");
+
+    return (
+        <div>
+            <nav className="navbar">
+                <h2>Sports Recommendation App</h2>
+
+                <div className="nav-buttons">
+                    <button
+                        className={currentPage === "sports" ? "active" : ""}
+                        onClick={() => setCurrentPage("sports")}
+                    >
+                        Sporturi
+                    </button>
+
+                    <button
+                        className={currentPage === "questionnaire" ? "active" : ""}
+                        onClick={() => setCurrentPage("questionnaire")}
+                    >
+                        Chestionar
+                    </button>
+                </div>
+            </nav>
+
+            {currentPage === "sports" && <SportsPage />}
+            {currentPage === "questionnaire" && <QuestionnairePage />}
+        </div>
+    );
 }
 
 export default App;
